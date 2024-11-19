2a1c760f17a5c99caa4e17c912e1ace0c7890613
Alejandro Sánchez Yalí <asanchezyali@gmail.com>
2024-11-19 12:46:26 -0500

    🎿 Update project configuration: add secret.txt to .gitignore, implement Lock smart contract, and create deployment and testing modules

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,43 +1,24 @@
 require("@nomicfoundation/hardhat-toolbox");
+
 const fs = require("fs");
-const privateKey = fs.readFileSync("secrete.txt").toString();
+const privateKey = fs.readFileSync("secret.txt").toString().trim();
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  defaultNetwork: "sepolia",
+  defaultNetwork: "localhost",
   networks: {
     hardhat: {
       chainId: 4202,
     },
     sepolia: {
-      url: "https://rpc.sepolia-api.lisk.com",
+      url: "https://sepolia.infura.io",
       accounts: [privateKey],
-      gasPrice: 1000000000,
-    },
-    BitTorrent: {
-      url: "https://pre-rpc.bt.io/",
-      accounts: [privateKey],
-      gasPrice: 1000000000,
-    },
-    zkEVMCardonaTestnet: {
-      url: "https://polygon-zkevm-cardona.blockpi.network/v1/rpc/public",
-      accounts: [privateKey],
-      gas: 6000000,
-      gasPrice: 20000000000,
-    },
-    mumbai: {
-      url: "https://rpc-mumbai.maticvigil.com",
-      accounts: [privateKey],
-    },
-    matic: {
-      url: "https://polygon-mumbai.g.alchemy.com/v2/2bGIFu-iEnl9RvAOTe1ddZI2gBnuYQGS",
-      accounts: [privateKey],
+      gasPrice: 10000000000,
     },
   },
-  solidity: "0.8.24",
+  solidity: "0.8.27",
   allowUnlimitedContractSize: true,
   throwOnTransactionFailures: true,
   throwOnCallFailures: true,
   loggingEnabled: true,
 };
-
-// npx hardhat ignition deploy ./ignition/modules/Lock.js --network BitTorrent
